d85430622923a47ab2c86a582c8720388aa1dbe7
ekildishev <47750057+ekildishev@users.noreply.github.com>
2019-05-26 22:55:26 +0300

    Update print.cpp

--- sources/print.cpp
+++ sources/print.cpp
@@ -5,7 +5,7 @@ void print(const std::string& text, std::ostream& out)
 	out << text;
 }
 
-void print(const std::string& text, std::ofstream out)
+void print(const std::string& text, std::ofstream& out)
 {
 	out << text;
 }
